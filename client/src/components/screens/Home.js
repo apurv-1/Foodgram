19d2e863a79118eed6f6e8c93eca26a7df53d86a
Apurv Gupta <apurv3351@gmail.com>
2020-10-29 10:42:52 +0530

    fixed bugs

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
 	const [data, setData] = useState([]);
-	const { state, dispatch } = useContext(UserContext);
+	const { state } = useContext(UserContext);
 	useEffect(() => {
 		fetch("/allpost", {
 			headers: {
@@ -121,71 +121,73 @@ const Home = () => {
 
 	return (
 		<div className="home">
-			{data.map((item) => {
-				return (
-					<div className="card home-card" key={item._id}>
-						<h5>
-							<div className="upper-profile">
-								<Link to={item.postedBy._id != state._id ? "/profile/" + item.postedBy._id : "/profile"}>
-									<img className="small-pic" src={state.pic} />
-									<span className="te">{item.postedBy ? item.postedBy.name : "loading"}</span>
-								</Link>
-								{item.postedBy._id == state._id && (
-									<i
-										className="material-icons"
-										style={{ float: "right", color: "red" }}
-										onClick={() => deletePost(item._id)}>
-										delete_forever
-									</i>
-								)}
-							</div>
-						</h5>
-						<div className="card-image">
-							<img src={item.photo} />
-						</div>
-						<div className="card-content">
-							{item.likes.includes(state._id) ? (
-								<i
-									className="material-icons"
-									style={{ color: "red" }}
-									onClick={() => {
-										unlikePost(item._id);
-									}}>
-									favorite
-								</i>
-							) : (
-								<i
-									className="material-icons"
-									onClick={() => {
-										likePost(item._id);
-									}}>
-									favorite_border
-								</i>
-							)}
-							<span className="likes">{item.likes.length} likes</span>
+			{state
+				? data.map((item) => {
+						return (
+							<div className="card home-card" key={item._id}>
+								<h5>
+									<div className="upper-profile">
+										<Link to={item.postedBy._id != state._id ? "/profile/" + item.postedBy._id : "/profile"}>
+											<img className="small-pic" src={state.pic} />
+											<span className="te">{item.postedBy ? item.postedBy.name : "loading"}</span>
+										</Link>
+										{item.postedBy._id == state._id && (
+											<i
+												className="material-icons"
+												style={{ float: "right", color: "red" }}
+												onClick={() => deletePost(item._id)}>
+												delete_forever
+											</i>
+										)}
+									</div>
+								</h5>
+								<div className="card-image">
+									<img src={item.photo} />
+								</div>
+								<div className="card-content">
+									{item.likes.includes(state._id) ? (
+										<i
+											className="material-icons"
+											style={{ color: "red" }}
+											onClick={() => {
+												unlikePost(item._id);
+											}}>
+											favorite
+										</i>
+									) : (
+										<i
+											className="material-icons"
+											onClick={() => {
+												likePost(item._id);
+											}}>
+											favorite_border
+										</i>
+									)}
+									<span className="likes">{item.likes.length} likes</span>
 
-							<p>
-								<span class="cap-name">{item.postedBy.name}</span>
-								{item.body}
-							</p>
-							{item.comments.map((record) => {
-								return (
-									<h6 key={record._id}>
-										<b>{record.postedBy ? record.postedBy.name : "loading.."}</b> {record.text}
-									</h6>
-								);
-							})}
-							<form
-								onSubmit={(e) => {
-									e.preventDefault();
-									makeComment(e.target[0].value, item._id);
-								}}>
-								<input type="text" placeholder="add a comment···" />
-							</form>
-						</div>
-					</div>
-				);
-			})}
+									<p>
+										<span class="cap-name">{item.postedBy.name}</span>
+										{item.body}
+									</p>
+									{item.comments.map((record) => {
+										return (
+											<h6 key={record._id}>
+												<b>{record.postedBy ? record.postedBy.name : "loading.."}</b> {record.text}
+											</h6>
+										);
+									})}
+									<form
+										onSubmit={(e) => {
+											e.preventDefault();
+											makeComment(e.target[0].value, item._id);
+										}}>
+										<input type="text" placeholder="add a comment···" />
+									</form>
+								</div>
+							</div>
+						);
+				  })
+				: "Loading..."}
 		</div>
 	);
 };
--- myapp.js
+++ myapp.js
@@ -1,39 +1,38 @@
-const express = require('express')
-const myapp = express()
-const mongoose = require('mongoose')
-const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const express = require("express");
+const myapp = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+const { MONGOURI } = require("./config/keys");
 
- 
+mongoose.connect(MONGOURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
+mongoose.connection.on("connected", () => {
+	console.log("Connected to Database!");
+});
+mongoose.connection.on("error", (err) => {
+	console.log("error connecting", err);
+});
 
-mongoose.connect(MONGOURI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-mongoose.connection.on('connected',()=>{
-    console.log("Connected to Database!")
-})
-mongoose.connection.on('error',(err)=>{
-    console.log("error connecting",err)
-})
+require("./models/user");
+require("./models/post");
 
-require('./models/user')
-require('./models/post')
+myapp.use(express.json());
+myapp.use(require("./routes/auth"));
+myapp.use(require("./routes/post"));
+myapp.use(require("./routes/user"));
 
-myapp.use(express.json())
-myapp.use(require('./routes/auth'))
-myapp.use(require('./routes/post'))
-myapp.use(require('./routes/user'))
-
-
-if(process.env.NODE_ENV=="production"){
-    myapp.use(express.static('client/build'))
-    const path = require('path')
-    myapp.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+if (process.env.NODE_ENV == "production") {
+	myapp.use(express.static("client/build"));
+	const path = require("path");
+	myapp.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
 }
 
-myapp.listen(PORT,()=>{
-    console.log("Server is running", PORT)
-})
+myapp.listen(PORT, () => {
+	console.log("Server is running", PORT);
+});
