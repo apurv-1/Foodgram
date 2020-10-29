5e9a1d50cdf5252fb8773b1eaa0d7b44ab78d849
Apurv Gupta <apurv3351@gmail.com>
2020-10-29 10:51:32 +0530

    fixed bugs

--- myapp.js
+++ myapp.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const { MONGOURI } = require("./config/keys");
 
+require("dotenv").config();
+
 mongoose.connect(MONGOURI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
